b1a2ca056810d704c8c6effc59710f6fcd786c21
Zack Newsham <zack.newsham@gmail.com>
2023-11-15 17:14:32 -0500

    Make fibers work on v18

--- deps/v8/src/execution/thread-id.cc
+++ deps/v8/src/execution/thread-id.cc
@@ -11,7 +11,8 @@ namespace internal {
 
 namespace {
 
-thread_local int thread_id = 0;
+DEFINE_LAZY_LEAKY_OBJECT_GETTER(base::Thread::LocalStorageKey, GetThreadIdKey,
+                                base::Thread::CreateThreadLocalKey())
 
 std::atomic<int> next_thread_id{1};
 
@@ -19,14 +20,18 @@ std::atomic<int> next_thread_id{1};
 
 // static
 ThreadId ThreadId::TryGetCurrent() {
+  int thread_id = base::Thread::GetThreadLocalInt(*GetThreadIdKey());
   return thread_id == 0 ? Invalid() : ThreadId(thread_id);
 }
 
 // static
 int ThreadId::GetCurrentThreadId() {
+  auto key = *GetThreadIdKey();
+  int thread_id = base::Thread::GetThreadLocalInt(key);
   if (thread_id == 0) {
     thread_id = next_thread_id.fetch_add(1);
     CHECK_LE(1, thread_id);
+    base::Thread::SetThreadLocalInt(key, thread_id);
   }
   return thread_id;
 }
